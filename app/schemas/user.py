8778edaa8762e94fed44411288aa89da3ecc9c02
Sumanth Reddy <SumanthReddy@MacBook-Pro.local>
2025-04-09 11:07:49 -0400

    Add schemas for user register/login/response

---
+++ app/schemas/user.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
