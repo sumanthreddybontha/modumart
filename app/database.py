5a1c07d0f6bf2db9ebb0fd654d15f82ae0954c93
sumanthreddybontha <sumanthredddy.bontha@gmail.com>
2025-04-14 22:34:45 -0400

    Hooked refresh token expiry to env var

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
